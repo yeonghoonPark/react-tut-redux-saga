e265213b751219fd82571e16a906cdd4506a29eb
yeonghoon Park <huklmnj2@naver.com>
2023-07-08 22:56:40 +0900

    React redux saga #11 Configure Saga

---
+++ src/redux/productSaga.js
@@ -0,0 +1,2 @@
+// function*은 generator function을 의미
+export default function* productSaga() {}
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,17 @@
 // import { createStore } from "redux";
 import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "./rootReducer";
+import productSaga from "./productSaga";
+import createSagaMiddleware from "@redux-saga/core";
+
+const sagaMiddleware = createSagaMiddleware();
 
 // const store = createStore(rootReducer);
-const store = configureStore({ reducer: rootReducer });
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: [sagaMiddleware],
+});
+
+sagaMiddleware.run(productSaga);
 
 export default store;
